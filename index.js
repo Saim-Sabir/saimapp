fddea901776ecfbcd81546c894646d5b773cb5c3
Saim-Sabir <132016143+Saim-Sabir@users.noreply.github.com>
2023-08-23 21:35:09 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const userrouter = require('./routes/userRouter.js');
+const screenrouter = require('./routes/screensRoute.js');
+const billingrouter = require('./routes/billingRoute.js');
+const withdrawrouter = require('./routes/withdrawRoute.js');
+const invoicerouter = require('./routes/invoiceRoute.js');
+const awardrouter = require("./routes/awardRoute.js");
+const messagerouter = require("./routes/messageRoute.js");
+const orderrouter = require("./routes/orderRoute.js");
+const contentwriterouter = require("./routes/contentwritingRoute.js");
+
+
+app.use(cors());
+app.use(express.json());
+require('./db/DB.js');
+app.use('/user', userrouter);
+app.use('/invoice', invoicerouter);
+app.use('/award', awardrouter);
+app.use('/screenshot', screenrouter);
+app.use('/withdraw', withdrawrouter);
+app.use('/billing', billingrouter);
+app.use('/message', messagerouter);
+app.use('/orders', orderrouter);
+app.use('/content', contentwriterouter);
+app.use('/uploads', express.static('./uploads'));
+
+
+const port = process.env.PORT || 9000 || 9071 ;
+
+app.listen(port, ()=>{
+    console.log('Server is running on port:', + port);
+});
