d10a938d77da63feee137027e352be4c5ce83b4b
Saim-Sabir <132016143+Saim-Sabir@users.noreply.github.com>
2023-08-23 21:47:53 -0700

    Delete contentwritingModel.js

--- models/contentwritingModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ContentwritingSchema = new mongoose.Schema({
-
-    topic : {type:String , required: true},
-    date : {type: Date}
-});
-const  ContentwritingDB = new mongoose.model("contentwrite", ContentwritingSchema);
-module.exports = ContentwritingDB;
