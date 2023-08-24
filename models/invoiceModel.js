9583a41388d591db874154c82d1744539ba86e4a
Saim-Sabir <132016143+Saim-Sabir@users.noreply.github.com>
2023-08-23 21:29:06 -0700

    Delete models directory

--- models/invoiceModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const invoiceSchema = new mongoose.Schema({
-    userId : {type:String, required: true},
-    amount : {type:Number, required: true},
-    date : {type: Date}
-});
-const InvoiceDb = new mongoose.model("invoice", invoiceSchema);
-module.exports = InvoiceDb;
